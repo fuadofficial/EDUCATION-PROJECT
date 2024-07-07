46847d3dfd9395be8a5988e0bb566862f1c1cd3e
fuad <ahmadufuadoorakam@gmail.com>
2024-07-07 22:49:46 +0530

    completed header section and footer section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header.js/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer/>
     </div>
   )
 }
--- src/components/Header.js/Header.js
+++ src/components/Header.js/Header.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import './Header.css'
+import logo from '../../assets/img/logo.png'
 
 function Header() {
+    const nav_title = [
+        { path: '/', display: 'Home' },
+        { path: '/', display: 'Services' },
+        { path: '/', display: 'Courses' },
+        { path: '/', display: 'About Us' }
+    ]
     return (
         <header className='header'>
             <div className="container">
                 <div className="nav_container">
                     <div className="logo">
                         <div className="logo-img">
-                            <img src="" alt="" />
+                            <img src={logo} alt="Techflix AI logo" />
                         </div>
-                        <h2>Techflix Ai</h2>
+                        <h2>Techflix AI</h2>
+                    </div>
+                    <div className="navigation">
+                        <ul className='menu'>
+                            {nav_title.map((item, index) => (
+                                <li className='nav_item' key={index}>{item.display}</li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
